14ce748bdafcbe55901b3818193b94cde7a78ae7
NirmitAgrawal02 <nagraw18@asu.edu>
2022-10-27 15:08:08 -0700

    Skeleton Code V1.0

---
+++ src/urinals.java
@@ -0,0 +1,28 @@
+/*
+Author - Nirmit Agrawal - nagraw18@asu.edu
+Course - SER 515
+
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class urinals {
+    ArrayList<String> input = new ArrayList<>();
+    public void getString()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String");
+
+    }
+    public void openFile()
+    {
+
+    }
+    public int count()
+    {
+        int count = 0;
+        return count;
+
+    }
+}
---
+++ test/urinalsTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class urinalsTest {
+
+    @Test
+    void getString() {
+    }
+
+    @Test
+    void openFile() {
+    }
+
+    @Test
+    void count() {
+    }
+}
