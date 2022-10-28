f90e2f0a5a2c67e7ea8d0247e8a4145dfe963cfc
NirmitAgrawal02 <nagraw18@asu.edu>
2022-10-27 17:18:33 -0700

    Function 1 -Solved Test Case 4 - Completed

--- test/UrinalsTest.java
+++ test/UrinalsTest.java
@@ -1,15 +1,15 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class urinalsTest {
+class UrinalsTest {
 
     @Test
     void getstring() {
         urinals test = new urinals();
-        assertEquals("Given String is a good string",test.getstring("00000"), "NIRMIT AGRAWAL,TEST ONE EXECUTED");
         assertEquals("Length of the given string exceeds the constraints",test.getstring("000001111110000000001"), "NIRMIT AGRAWAL,TEST TWO EXECUTED");
         assertEquals("Given String does not contain any urinals",test.getstring(""), "NIRMIT AGRAWAL,TEST TWO EXECUTED");
-
+        assertEquals("Given String is not a good string",test.getstring("421145"), "NIRMIT AGRAWAL,TEST TWO EXECUTED");
+        assertEquals("Given String is a good string",test.getstring("00000"), "NIRMIT AGRAWAL,TEST ONE EXECUTED");
     }
 
     @Test
